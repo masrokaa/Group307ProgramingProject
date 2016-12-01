91c10cdc57c6f1bb638439b144f9f105c98f69a2
sennepsild <magnusmhp@gmail.com>
2016-12-01 11:05:09 +0100

    More MultiPlayer lobby stuff

--- Assets/LobbyBehaviour.cs
+++ Assets/LobbyBehaviour.cs
@@ -5,10 +5,11 @@ using UnityEngine.UI;
 public class LobbyBehaviour : MonoBehaviour {
     public Text player1, player2, player3, player4;
     bool ishost;
-
+    UnityClient multiplayerInfo;
 
 	// Use this for initialization
 	void Start () {
+        multiplayerInfo = (UnityClient) GameObject.FindObjectOfType<UnityClient>();
         ishost = UnityClient.isHost;
         Debug.Log(ishost);
 	}
@@ -21,6 +22,23 @@ public class LobbyBehaviour : MonoBehaviour {
 	}
     void updatePlayers()
     {
-
+        int numberOfPlayers = 0;
+        numberOfPlayers = multiplayerInfo.GetNumberOfPlayersInLobby();
+        if (numberOfPlayers > 0)
+            player1.text = "Green Player";
+        else
+            player1.text = "";
+        if (numberOfPlayers > 1)
+            player2.text = "Red Player";
+        else
+            player2.text = "";
+        if (numberOfPlayers > 2)
+            player3.text = "Blue Player";
+        else
+            player3.text = "";
+        if (numberOfPlayers > 3)
+            player4.text = "Yellow Player";
+        else
+            player4.text = "";
     }
 }
