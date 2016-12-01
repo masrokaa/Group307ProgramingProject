428947d49f4c1d55e5d78a1d420fbbcec9ac408d
jorik990 <roofman990@gmail.com>
2016-12-01 14:26:08 +0100

    boardPlacement, clickObject

---
+++ Assets/Georgi/clickObject.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class script : MonoBehaviour {
+
+    public int mouseClicks;
+
+
+    // Use this for initialization
+    void Start () {
+        writeText = GameObject.Find("DiceText").GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            int dice = Random.Range(1, 7);
+            mouseClicks += 1;
+        }
+    }
+
+    public void WriteToTextField()
+    {
+        
+
+        writeText.text = "" + rndDice;
+
+    }
+}
